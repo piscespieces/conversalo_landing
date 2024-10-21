78f17beb30659b0bf82d71c47d19674fe76b1aab
piscespieces <piscespieces@users.noreply.github.com>
2024-10-21 22:13:38 +0000

    deploy: f5d08515b15e29c5f66e2255dde00d19211a908a

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,20 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.remove('opacity-0');
+      entry.target.classList.add('opacity-100');
+    }
+  });
+}, {
+  threshold: 0.1 // Trigger when 10% of the element is visible
+});
+
+// Select the sections to observe
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+  observer.observe(section);
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('email-form').addEventListener('submit', function(event) {
     event.preventDefault();
